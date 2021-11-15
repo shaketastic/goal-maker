bb74091158850fa38c4849c5e86b89a385f11173
shareekaepps <shareekaepps@icloud.com>
2021-11-15 18:39:58 -0500

    added code, then will pull

--- src/components/Note.js
+++ src/components/Note.js
@@ -4,16 +4,10 @@ class Note extends React.Component {
   render() {
     return (
       <div className="note">
-        <h4>Title</h4>
-        <div>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
-          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-          aliquip ex ea commodo consequat. Duis aute irure dolor in
-          reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-          pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
-          culpa qui officia deserunt mollit anim id est laborum.
-        </div>
+        <div>{this.props.myNote}</div>
+        <button onClick={() =>this.props.handleDeleteNote(this.props.myNote)} >
+        X
+        </button>
       </div>
     );
   }
--- src/components/Notes.js
+++ src/components/Notes.js
@@ -2,21 +2,55 @@ import React from "react";
 import Note from "./Note";
 
 class Notes extends React.Component {
+  constructor(){
+    super();
+    this.state = {
+      noteInput: "",
+      notes: [],
+    };
+  }
+
+  handleNoteInput = (event) => {
+    this.setState({
+      noteInput: event.target.value, 
+  });
+};
+
+  handleAddNote = () => {
+    this.setState({
+      notes: [...this.state.notes, this.state.noteInput],
+      noteInput: "",
+    });
+  };
+
+  handleDeleteNote = (noteToDelete) => {
+    let filteredNotes = this.state.notes.filter((note) => {
+      return note != noteToDelete
+    })
+    this.setState({
+      notes: filteredNotes,
+    });
+  };
+
   render() {
+    let notToDisplay = this.state.notes.map((note, i) => {
+      return <Note 
+      key={note + i} 
+      myNote={note} 
+      handleDeleteNote={this.handleDeleteNote} />
+    });
+
     return (
       <div className="notes">
         <h2>Notes</h2>
-        <input type="text" />
-        <button>Add note</button>
-        <div className="note-container">
-          <Note />
-          <Note />
-          <Note />
-          <Note />
+        <textarea 
+          value={this.state.noteInput}
+          onChange={this.handleNoteInput} />
+        <button onClick={this.handleAddNote}>Add note</button>
+        <div className="note-container">{notesToDisplay}</div>
         </div>
-      </div>
-    );
+      );
+    }
   }
-}
 
 export default Notes;
